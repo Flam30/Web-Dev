a224f8c9ce84b1625968c7d94db6bcf023550cfd
Teo Portase <teoportase@gmail.com>
2023-09-14 01:13:40 +0200

    #2 Add venue endpoints

--- server/controllers/tickets.js
+++ server/controllers/tickets.js
@@ -1,1 +1,10 @@
-// empty for now
+var express = require('express');
+var router = express.Router();
+var Event = require('../models/ticket');
+
+/*
+    POST /organizers/:id/events/:id/tickets - add tickets to an event
+    GET /organizers/:id/events/:id/tickets - get specific event's tickets
+    GET /organizers/:id/events/:id/tickets/:id - get a specific ticket from an event
+    DELETE /organizers/:id/events/:id/tickets/:id - delete a specific ticket from an event
+*/
--- server/controllers/venues.js
+++ server/controllers/venues.js
@@ -1,1 +1,42 @@
-// empty for now
+var express = require('express');
+var router = express.Router();
+var Venue = require('../models/venue');
+
+router.post('/api/venues', async function(req, res, next) {
+    var venue = new Venue(req.body);
+    try {
+        await venue.save();
+        res.status(201).json(venue);
+    } catch (err) {
+        next(err);
+    }
+});
+
+router.get('/api/venues', async (req, res, next) => {
+    try {
+        const venues = await Venue.find({});
+        if (venues.length === 0) {
+            return res.status(204).json({ 'message': 'No venues exist.' });
+        }
+
+        res.send(venues);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+router.delete('/api/venues/:id', async function(req, res, next){
+    console.log('here');
+    var id = req.params.id;
+    try {
+        const venue = await Venue.findOneAndDelete({_id: id});
+        if (venue === null) {
+            return res.status(404).json({'message': 'Venue not found!'});
+        }
+        res.json(venue);
+    } catch (err) {
+        return next(err);
+    }
+});
+
+module.exports = router;
