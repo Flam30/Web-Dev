322061afe3fab55e7fe6c0aaa06ef4690b1d6252
Teo Portase <teoportase@gmail.com>
2023-09-21 17:17:58 +0200

    #2 Add preliminary foreign key to ticket

--- server/models/ticket.js
+++ server/models/ticket.js
@@ -6,7 +6,9 @@ var ticketSchema = new Schema({
     type: {type: String},
     seat: {type: String},
     price: {type: Number},
-    availability: {type: Boolean}
+    availability: {type: Boolean},
+    event: {type: Schema.Types.ObjectId, ref: 'event', required: true}
+    // customer: {type: Schema.Types.ObjectId, ref: 'customer'}
 });
 
 module.exports = mongoose.model('tickets', ticketSchema);
