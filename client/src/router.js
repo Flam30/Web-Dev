24ba2c5db5117479ac6806a4f80f5c698bb82bcb
Adrian Hassa <adrianhassa@gmail.com>
2023-09-28 18:22:25 +0200

    #5 Create Home page skeleton

--- client/src/router.js
+++ client/src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Event from './views/Event.vue'
 
 Vue.use(Router)
 
@@ -12,6 +13,12 @@ export default new Router({
       path: '/',
       name: 'home',
       component: Home
+    },
+    {
+      path: '/event/:id',
+      props: true,
+      name: 'Event',
+      component: Event
     }
   ]
 })
---
+++ client/src/views/Event.vue
@@ -0,0 +1,31 @@
+<script>
+import HeaderBar from '@/components/HeaderBar'
+
+export default {
+  name: 'Event',
+  data() {
+    return {
+      message: 'none'
+    }
+  },
+  components: {
+    HeaderBar
+  },
+  props: {
+    id: String
+  },
+  methods: {
+  }
+}
+</script>
+
+<template>
+<div>
+    <HeaderBar></HeaderBar>
+    <h1>{{ id }}</h1>
+</div>
+</template>
+
+<style>
+
+</style>
