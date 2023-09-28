cbd02e3a17e983fa843627d0939e9feef94eb637
Teo Portase <teoportase@gmail.com>
2023-09-28 19:11:47 +0200

    #4 Change sample requests

--- server/models/event.js
+++ server/models/event.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var eventSchema = new Schema({
-    id: {type: Number, unique: true, required: true},
+    id: {type: String, unique: true, required: true},
     organizer: {type: Schema.Types.ObjectId, ref: 'organizers', required: true},
     name: {type: String},
     //venue: {type: Schema.Types.ObjectId, ref: 'venues', required: true},
--- server/models/venue.js
+++ server/models/venue.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var venueSchema = new Schema({
-    id: {type: Number, unique: true, required: true},
+    id: {type: String, unique: true, required: true},
     name: {type: String},
     location: {type: String},
     numberOfSeats: {type: Number}
