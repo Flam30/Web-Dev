1bb7c0340d6a337b996095f98f3ca4d5bdaa640e
Adrian Hassa <adrianhassa@gmail.com>
2023-10-16 17:18:59 +0200

    Fix ticket controllers

--- server/controllers/v1/tickets.js
+++ server/controllers/v1/tickets.js
@@ -1,18 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var Ticket = require('../../models/ticket');
+const stripe = require('stripe')('sk_test_51NvnEZIeSorUA2wFhH58TE9WBEHysbpdbxcdxZuMIIugCStAChIXEyDKGaha1DCkD3vCbrlL13ypQh7NU3x1KzGl00WCUbbxYd');
 
 // POST /events/:eventId/tickets - add new ticket to existing event
 router.post('/', async function(req, res, next) {
     try {
         const eventId = req.params.eventId;
-        let ticket = new Ticket({
-            id: req.body.id,
-            seat: req.body.seat || "",
-            price: req.body.price,
-            quantity: req.body.quantity,
-            event: eventId || req.body.event
-        });
+        const product = await stripe.products.create({
+            name: req.body.id,
+            default_price_data: {
+                currency: 'SEK',
+                unit_amount_decimal: req.body.price * 100
+            }
+          });
+    
+        let ticket = new Ticket(req.body);
+        ticket.price = product.default_price;
         ticket._id = ticket.id;
 
         await ticket.save();
